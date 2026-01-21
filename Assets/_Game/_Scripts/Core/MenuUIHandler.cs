4eac51f089503107596e18f891ae0050f586c476
nazzony <nazar.shpyt@gmail.com>
2026-01-21 18:19:32 +0200

    Updated Settings Menu As Panel

--- Assets/_Game/_Scripts/Core/MenuUIHandler.cs
+++ Assets/_Game/_Scripts/Core/MenuUIHandler.cs
@@ -5,6 +5,10 @@ using UnityEditor;
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject SettingsPanel;
+    private bool isActive = false;
+
     // Buttons Handling
     public void ReturnToMainMenu()
     {
@@ -19,14 +23,15 @@ public class MenuUIHandler : MonoBehaviour
     {
         SceneManager.LoadScene((int)Scenes.Scene1);
     }
-    public void GoToSettings()
+    public void OpenSettings()
     {
-        if (Time.timeScale != 1f)
-        {
-            Time.timeScale = 1f;
-            GameIsPaused = false;
-        }
-        SceneManager.LoadScene((int)Scenes.Settings);
+        SettingsPanel.SetActive(true);
+        isActive = true;
+    }
+    public void CloseSettings()
+    {
+        SettingsPanel.SetActive(false);
+        isActive = false;
     }
     
     public void Exit()
