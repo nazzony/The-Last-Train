a765c03f5d417b158f84df96e1c2f4bed68654b4
Avtobu <lifeivanna2007@gmail.com>
2026-01-21 13:47:54 +0200

    Added file for dragging objects

---
+++ Assets/_Game/_Scripts/Data/DraggableItem.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generatic;
+using UnityEngine.EventSystem;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
