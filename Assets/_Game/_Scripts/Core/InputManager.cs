a7e85026df03a3227e69da106f327b1efaafe6ad
nazzony <nazar.shpyt@gmail.com>
2026-01-22 20:27:42 +0200

    Upgraded DoorTransition With Distance Control & Ignore Of Raycasts for PLayer

--- Assets/_Game/_Scripts/Core/InputManager.cs
+++ Assets/_Game/_Scripts/Core/InputManager.cs
@@ -1,20 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class InputManager : MonoBehaviour
 {
+    public Transform playerTransform;
+    public Transform doorsTransform;
     void Update()
     {
         if (Time.timeScale == 0f) return;
+
         if(Input.GetMouseButtonDown (0))
         {
             Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D isHit = Physics2D.Raycast(worldPos, Vector2.zero);
-            if(isHit.collider != null && isHit.collider.CompareTag("Ground"))
+            float distance = Vector2.Distance(playerTransform.position, doorsTransform.position);
+
+            if (isHit.collider != null && isHit.collider.CompareTag("Ground"))
             {
                 EventManager.current.TriggerPlayerMove(isHit.point);
             } 
-            else if (isHit.collider != null && isHit.collider.CompareTag("Door"))
+
+            else if (isHit.collider != null && isHit.collider.CompareTag("Door") && distance <= 2.3f)
             {
                 DoorHandling number = isHit.collider.GetComponent<DoorHandling>();
                 if (number != null)
